a01c5a97d18ecb9bda06875ee777a5ad9981c079
SkyLeggend <91502907+SkyLeggend@users.noreply.github.com>
2025-11-21 11:16:23 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import HomeMainPage from "./(homes)/home-main/home.tsx";
+import HomeMainPage from "./(homes)/home-main/home";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title:
